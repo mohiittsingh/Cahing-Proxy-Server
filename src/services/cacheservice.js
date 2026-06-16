36369d2207ea94ce009831452d5f725460c283d0
mohiittsingh <mohiittsingh@gmail.com>
2026-06-16 08:21:27 +0530

    cache services

--- src/services/cacheservice.js
+++ src/services/cacheservice.js
@@ -1,2 +1,12 @@
 import redis from "../config/redis"
 import env from "../config/env"
+
+async function get(key){
+return await redis.get(key);
+}
+
+async function set(key,value){
+    await redis.set(
+        
+    )
+}
