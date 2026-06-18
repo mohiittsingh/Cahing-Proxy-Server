3467eb6ec0d50539923b5895025c7b922f346a76
mohiittsingh <mohiittsingh@gmail.com>
2026-06-18 16:21:17 +0530

    new files added

--- src/middleware/cache.middleware.js
+++ src/middleware/cache.middleware.js
@@ -13,4 +13,20 @@ NO
 Continue to API  */
 
 import cacheservice from "../services/cacheservice"
-import cachekey from "../utils/cachekey"
+import generatecachekey from "../utils/cachekey"
+import { cachekey } from '..';
+
+async function cacheMiddleware(req,res,next){
+    const cache=generatecachekey(req);
+    const cachedData = await cacheservice.get(cacheKey);
+    if(cachedData){
+        res.set('X-cache',"Hit")
+        return res.json(cachedData)
+    }
+    res.locals.cacheKey = cacheKey;
+
+    res.set('X-Cache', 'MISS');
+
+    next();module.exports = cacheMiddleware;
+
+}
